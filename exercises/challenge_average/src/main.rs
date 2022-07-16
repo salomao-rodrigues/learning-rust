3f9ea23697638ded91980012f7d874aa4e5f9720
Salomao Rodrigues <salomao88@gmail.com>
2022-07-16 15:39:51 +0100

    Solve average challenge

--- exercises/challenge_average/src/main.rs
+++ exercises/challenge_average/src/main.rs
@@ -3,7 +3,7 @@ fn main() {
     let b = 2.3;
     let c: f32 = 120.0;
 
-    /* YOUR CODE GOES HERE */
+    let average = (a as f64 + b + c as f64) / 3.0;
 
     assert_eq!(average, 45.1);
     println!("Test passed!");
