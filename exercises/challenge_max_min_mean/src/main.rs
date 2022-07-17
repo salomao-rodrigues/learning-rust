c1ffdea224e4bee1ce28f50fbfb8e171783e644b
Salomao Rodrigues <salomao88@gmail.com>
2022-07-17 12:41:54 +0100

    Solve max-min-mean challenge

---
+++ exercises/challenge_max_min_mean/src/main.rs
@@ -0,0 +1,23 @@
+fn main() {
+    let numbers = [1, 9, -2, 0, 23, 20, -7, 13, 37, 20, 56, -18, 20, 3];
+    let mut max: i32;
+    let mut min: i32;
+    let mut mean: f64;
+
+    min = i32::MAX;
+    max = i32::MIN;
+    mean = 0.0;
+
+    for number in numbers {
+        min = if min > number { number } else { min };
+        max = if max < number { number } else { max };
+        mean += number as f64
+    }
+
+    mean /= numbers.len() as f64;
+
+    assert_eq!(max, 56);
+    assert_eq!(min, -18);
+    assert_eq!(mean, 12.5);
+    println!("Tests passed!");
+}
